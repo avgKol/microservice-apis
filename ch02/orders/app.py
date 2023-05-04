ec5a310839f25f64b96d504340c8032501592c8b
avg <anantavijayguha@gmail.com>
2023-05-03 23:06:49 -0400

    added more comments

--- ch02/orders/app.py
+++ ch02/orders/app.py
@@ -1,6 +1,8 @@
+# Import the FastAPI module/library
 from fastapi import FastAPI
 
+# Create a new instance of FastAPI and enable debug mode
 app = FastAPI(debug=True)
 
-
+# Import the orders API module into the app (assumed to be in the orders/api folder)
 from orders.api import api

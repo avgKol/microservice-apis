dbb60e46a66bebf61e9e64f453774d3147d3cc0d
avg <anantavijayguha@gmail.com>
2023-05-03 23:10:33 -0400

    fixed extra code copied accidentally

--- ch02/orders/api/schemas.py
+++ ch02/orders/api/schemas.py
@@ -1,24 +1,3 @@
-from datetime import datetime
-from enum import Enum
-from typing import List, Optional
-from uuid import UUID
-
-from pydantic import BaseModel, conint, validator, conlist
-
-
-class Size(Enum):
-    small = "small"
-    medium = "medium"
-    big = "big"
-
-
-class StatusEnum(Enum):
-    created = "created"
-    paid = "paid"
-    progress = "progress"
-    cancelled = "cancelled"
-    dispatched = "dispatched"
-    delivered = "delivered"
 # Importing required modules
 from datetime import datetime
 from enum import Enum
@@ -66,30 +45,5 @@ class GetOrderSchema(CreateOrderSchema):
     status: StatusEnum
 
 # Defining a Pydantic model to represent multiple orders at once
-class GetOrdersSchema(BaseModel):
-    orders: List[GetOrderSchema]
-
-
-class OrderItemSchema(BaseModel):
-    product: str
-    size: Size
-    quantity: Optional[conint(ge=1, strict=True)] = 1
-
-    @validator("quantity")
-    def quantity_non_nullable(cls, value):
-        assert value is not None, "quantity may not be None"
-        return value
-
-
-class CreateOrderSchema(BaseModel):
-    order: conlist(OrderItemSchema, min_items=1)
-
-
-class GetOrderSchema(CreateOrderSchema):
-    id: UUID
-    created: datetime
-    status: StatusEnum
-
-
 class GetOrdersSchema(BaseModel):
     orders: List[GetOrderSchema]

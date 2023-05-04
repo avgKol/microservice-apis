678d26ad324db4b89d49eaea67c44e1c9b190ce0
avg <anantavijayguha@gmail.com>
2023-05-03 22:33:50 -0400

    added comments using chatgpt

--- ch02/orders/api/api.py
+++ ch02/orders/api/api.py
@@ -1,3 +1,4 @@
+# Import necessary libraries and modules
 import uuid
 from datetime import datetime
 from uuid import UUID
@@ -13,14 +14,15 @@ from orders.api.schemas import (
     GetOrdersSchema,
 )
 
+# Initialize an empty list to store orders
 orders = []
 
-
+# Define the get_orders route to fetch all orders
 @app.get("/orders", response_model=GetOrdersSchema)
 def get_orders():
     return {"orders": orders}
 
-
+# Define the create_order route to create a new order
 @app.post(
     "/orders",
     status_code=status.HTTP_201_CREATED,
@@ -34,7 +36,7 @@ def create_order(order_details: CreateOrderSchema):
     orders.append(order)
     return order
 
-
+# Define the get_order route to fetch a single order by its ID
 @app.get("/orders/{order_id}", response_model=GetOrderSchema)
 def get_order(order_id: UUID):
     for order in orders:
@@ -42,7 +44,7 @@ def get_order(order_id: UUID):
             return order
     raise HTTPException(status_code=404, detail=f"Order with ID {order_id} not found")
 
-
+# Define the update_order route to update an existing order by its ID
 @app.put("/orders/{order_id}", response_model=GetOrderSchema)
 def update_order(order_id: UUID, order_details: CreateOrderSchema):
     for order in orders:
@@ -51,7 +53,7 @@ def update_order(order_id: UUID, order_details: CreateOrderSchema):
             return order
     raise HTTPException(status_code=404, detail=f"Order with ID {order_id} not found")
 
-
+# Define the delete_order route to delete an existing order by its ID
 @app.delete(
     "/orders/{order_id}",
     status_code=status.HTTP_204_NO_CONTENT,
@@ -64,7 +66,7 @@ def delete_order(order_id: UUID):
             return
     raise HTTPException(status_code=404, detail=f"Order with ID {order_id} not found")
 
-
+# Define the cancel_order route to cancel an existing order by its ID
 @app.post("/orders/{order_id}/cancel", response_model=GetOrderSchema)
 def cancel_order(order_id: UUID):
     for order in orders:
@@ -73,7 +75,7 @@ def cancel_order(order_id: UUID):
             return order
     raise HTTPException(status_code=404, detail=f"Order with ID {order_id} not found")
 
-
+# Define the pay_order route to update the status of an existing order to "progress" by its ID
 @app.post("/orders/{order_id}/pay", response_model=GetOrderSchema)
 def pay_order(order_id: UUID):
     for order in orders:
--- ch02/orders/api/schemas.py
+++ ch02/orders/api/schemas.py
@@ -19,6 +19,55 @@ class StatusEnum(Enum):
     cancelled = "cancelled"
     dispatched = "dispatched"
     delivered = "delivered"
+# Importing required modules
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
+from uuid import UUID
+
+# Importing Pydantic classes for data validation and serialization
+from pydantic import BaseModel, conint, validator, conlist
+
+# Defining an Enum class to represent the different sizes of a product
+class Size(Enum):
+    small = "small"
+    medium = "medium"
+    big = "big"
+
+# Defining an Enum class to represent the different status of an order
+class StatusEnum(Enum):
+    created = "created"
+    paid = "paid"
+    progress = "progress"
+    cancelled = "cancelled"
+    dispatched = "dispatched"
+    delivered = "delivered"
+
+# Defining a Pydantic model to represent an item inside an order
+class OrderItemSchema(BaseModel):
+    product: str
+    size: Size
+    quantity: Optional[conint(ge=1, strict=True)] = 1
+    
+    # Defining a validator method to ensure the value of quantity cannot be None and must be greater than or equal to 1
+    @validator("quantity")
+    def quantity_non_nullable(cls, value):
+        assert value is not None, "quantity may not be None"
+        return value
+
+# Defining a Pydantic model to represent a complete order
+class CreateOrderSchema(BaseModel):
+    order: conlist(OrderItemSchema, min_items=1)
+
+# Defining a Pydantic model to represent an order that has already been created
+class GetOrderSchema(CreateOrderSchema):
+    id: UUID
+    created: datetime
+    status: StatusEnum
+
+# Defining a Pydantic model to represent multiple orders at once
+class GetOrdersSchema(BaseModel):
+    orders: List[GetOrderSchema]
 
 
 class OrderItemSchema(BaseModel):
